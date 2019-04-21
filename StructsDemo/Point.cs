26259d80e1b6948be94e9400ad41e99bb2353cdb
ltvch <HrenHrenov@myrambler.ru>
2019-04-21 21:41:18 +0300

    Create Point.cs

---
+++ StructsDemo/Point.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190420_StructsDemo
+{
+    struct Point
+    {
+        public int X;
+        public int Y;
+        public ConsoleColor Color;
+    }
+}
