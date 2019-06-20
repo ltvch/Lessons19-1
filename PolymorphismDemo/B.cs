2cb9cf3bb31727ef664460daf859c59fa847234f
ltvch <HrenHrenov@myrambler.ru>
2019-06-20 21:42:50 +0300

    Create B.cs

---
+++ PolymorphismDemo/B.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190601_InheritanceDemoII
+{
+    class B : A
+    {
+        public void f()
+        {
+            Console.WriteLine("B.f()");
+        }
+
+        public void fFromA()
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("B.fFromA()");
+            base.f();
+
+            Console.ForegroundColor = oldColor;
+        }
+
+        public override void fVirt()
+        {
+            Console.WriteLine("B.fVirt()");
+        }
+
+        public override string ToString()
+        {
+            return "class B";
+        }
+    }
+}
