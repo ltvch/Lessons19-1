21370e15aaca0b76511b628f945c20548892a7e7
ltvch <HrenHrenov@myrambler.ru>
2019-06-20 21:36:23 +0300

    Create A.cs

---
+++ InheritanceDemo2/A.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190601_InheritanceDemoII
+{
+    class A
+    {
+        public void f()
+        {
+            Console.WriteLine("A.f()");
+        }
+    }
+
+}
