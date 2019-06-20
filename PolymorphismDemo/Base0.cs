59a02bdec805aef26398f01cd02d6585c34ca12f
ltvch <HrenHrenov@myrambler.ru>
2019-06-20 21:43:44 +0300

    Create Base0.cs

---
+++ PolymorphismDemo/Base0.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20190601_InheritanceDemoII
+{
+    public abstract class Base0
+    {
+        public abstract void fVirt();
+    }
+}
