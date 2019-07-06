96e7c5454c5480eb6cb0cd3084aecede2b5c8cd9
ltvch <HrenHrenov@myrambler.ru>
2019-07-06 12:54:09 +0300

    Create I2.cs

---
+++ ImplicitInterface/I2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190706_ImplicitInterfacesImplDemo
+{
+    interface I2
+    {
+        void f();
+    }
+}
